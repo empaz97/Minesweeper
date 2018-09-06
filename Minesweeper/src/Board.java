2dde538ce7303b24eb2ea73199b8556ceb784d78
Emily Pazienza <emp88@drexel.edu>
2018-09-05 21:38:26 -0400

    Commenting for Game and Board java files

--- Minesweeper/src/Board.java
+++ Minesweeper/src/Board.java
@@ -1,3 +1,9 @@
+/*
+ * Board.java includes the Board class which is a JPanel representing the game board
+ * Author: Emily Pazienza
+ * Created: 08/24/2018
+ */
+
 import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
@@ -8,30 +14,52 @@ import java.util.concurrent.*;
 
 public class Board extends JPanel {
 
+    // options and settings for the current board
     private int width, height, nMines;
-    private int remainingMines, squaresRemaining;
-    private ArrayList<ArrayList<Square>> squares;
-    private ArrayList<Square> mines;
-    private Game game;
-    private boolean isFinished, isStarted;
     private boolean marks, color, sound;
     private String difficulty;
 
+    // variable attributes based on state of game
+    private int remainingMines, squaresRemaining;
+    private boolean isFinished, isStarted;
+
+    // a matrix of Squares which make up the board
+    private ArrayList<ArrayList<Square>> squares;
+    // a list of references to the mines
+    private ArrayList<Square> mines;
+
+    // the Game this board belongs to
+    private Game game;
+
+    /**
+     * Creates a new Board
+     * @param game - the parent Game that this Board belongs to
+     */
     public Board(Game game) {
         super();
 
+        this.setBackground(Color.lightGray);
         this.game = game;
+
+        // set up the default settings
         this.squares = new ArrayList<ArrayList<Square>>();
         this.mines = new ArrayList<Square>();
         this.marks = true;
         this.color = true;
         this.sound = false;
         this.setDifficulty("intermediate");
+
+        // let the game know how many mines we start with
         this.game.getMineCount().setVal(nMines);
 
+        // start a game!
         this.newGame();
     }
 
+    /**
+     * Takes a string with a difficulty and sets the options for that difficulty
+     * @param diff - a String indicating the difficulty to be set to
+     */
     public void setDifficulty (String diff) {
         this.difficulty = diff;
         switch (diff) {
@@ -44,29 +72,49 @@ public class Board extends JPanel {
             case "expert":
                 this.setOptions(24,24,99);
                 break;
-            default:
+            default: // if it doesn't match a difficulty, leave the settings as-is
                 break;
         }
 
+        // start a new game once we change settings
         this.newGame();
+
+        // resize the window based on the board dimensions
         this.game.pack();
     }
 
-    private void setOptions(int w, int h, int nM) {
-        this.width = w;
-        this.height = h;
-        this.nMines = nM;
-    }
-
-    public void setCustom (int w, int h, int n) {
+    /**
+     * Sets the difficulty to custom options
+     * @param w - width of the board
+     * @param h - height of the board
+     * @param n - number of mines on the board
+     */
+    public void setDifficulty (int w, int h, int n) {
         this.difficulty = "custom";
         this.setOptions(w,h,n);
         this.newGame();
         this.game.pack();
     }
 
+    /**
+     * Sets the game options to given values
+     * @param w - width of the board
+     * @param h - height of the board
+     * @param n - number of mines on the board
+     */
+    private void setOptions(int w, int h, int n) {
+        this.width = w;
+        this.height = h;
+        this.nMines = n;
+    }
+
+    /**
+     * Toggles the color/bw option for the board
+     */
     public void toggleColor() {
         this.color = !this.color;
+
+        // go through all of the squares, and change the image path suffix
         for (int i = 0; i < this.height; i++) {
             for (int j = 0; j < this.width; j++) {
                 if (this.color)
@@ -77,9 +125,13 @@ public class Board extends JPanel {
         }
     }
 
+    /**
+     * Toggles whether or not the game includes marks
+     */
     public void toggleMarks() {
         this.marks = !this.marks;
         if (!this.marks) {
+            // if we turned off marks, we go through and unmark all marked squares
             for (int i = 0; i < this.height; i++) {
                 for (int j = 0; j < this.width; j++) {
                     if (this.squares.get(i).get(j).getIsMarked()) {
@@ -90,36 +142,43 @@ public class Board extends JPanel {
         }
     }
 
+    /**
+     * Toggles whether sound effects are played
+     */
     public void toggleSound() {
         this.sound = !this.sound;
     }
 
-    public boolean getHasMarks() {
-        return this.marks;
-    }
-
-    public boolean getIsColored() {
-        return this.color;
-    }
-
+    /**
+     * Begins a new game with the currently set options
+     */
     public void newGame() {
+
+        // stop the timer until the user starts playing and reset it to 0
         this.game.getTimer().stop();
         this.game.getTimeCount().setVal(0);
+
+        // set the new layout based on the current height and width
         this.setLayout(new GridLayout(this.height, this.width));
+
+        // remove and clear all components and settings
         this.removeAll();
         this.squares.clear();
         this.mines.clear();
         this.isFinished = false;
         this.isStarted = false;
 
+        // reset mine and square counts
         this.remainingMines = this.nMines;
         this.game.getMineCount().setVal(this.remainingMines);
         this.squaresRemaining = (this.width * this.height) - this.nMines;
 
+        // create the matrix of squares
         for (int h = 0; h < this.height; h++) {
+            // create a new row
             ArrayList<Square> row = new ArrayList<Square>();
             for (int w = 0; w < this.width; w++) {
-                //add a square
+                // create a new square
                 Square sq = new Square(this, w, h);
                 this.add(sq);
                 row.add(sq);
@@ -127,20 +186,28 @@ public class Board extends JPanel {
             this.squares.add(row);
         }
 
+        // populate the matrix with some mines
         this.addMines();
+
+        // redraw everything!
         this.revalidate();
         this.repaint();
     }
 
+    /**
+     * Populates the current matrix with mines
+     */
     private void addMines() {
         for (int i = 0; i < this.nMines; i++) {
+            // get a random square in the matrix
             int h = ThreadLocalRandom.current().nextInt(0, this.height);
             int w = ThreadLocalRandom.current().nextInt(0, this.width);
             Square s = this.squares.get(h).get(w);
 
+            // if there is already a mine, we can't add a second one
             if (s.getHasMine()) {
                 i--;
-            } else {
+            } else { // otherwise, add a mine and set it's adjacents
                 s.setMine();
                 this.mines.add(s);
                 this.chain("add adj", w, h);
@@ -148,6 +215,19 @@ public class Board extends JPanel {
         }
     }
 
+    /**
+     * Sets the game as started and begins the timer
+     */
+    public void start() {
+        this.isStarted = true;
+        this.game.getTimer().start();
+    }
+
+    /**
+     * Chains a certain action to all of a given location's neighbors
+     * @param action - the action to do
+     * @param x, y - the location to chain around
+     */
     public void chain(String action, int x, int y) {
         int north, south, east, west;
         north = y - 1;
@@ -155,62 +235,95 @@ public class Board extends JPanel {
         east = x + 1;
         west = x - 1;
 
+        // perform the action on the neighbors that share a side
         this.performAction(action, north, x);
         this.performAction(action, south, x);
         this.performAction(action, y, east);
         this.performAction(action, y, west);
 
+        // perform the action on the corners
         this.performAction(action, north, west);
         this.performAction(action, north, east);
         this.performAction(action, south, west);
         this.performAction(action, south, east);
     }
 
+    /**
+     * Performs a given action on the given location
+     * @param action - the action to be done
+     * @param y, x - the matrix location to be acted upon
+     */
     private void performAction(String action, int y, int x) {
+        // first check that the location exists in the matrix
         if (y >= 0 && x >= 0 && y < this.height && x < this.width) {
-            if (action.equals("open"))
+            // perform the requested action
+            if (action.equals("open")) {
                 this.squares.get(y).get(x).press();
-            if (action.equals("add adj"))
+            } else if (action.equals("add adj")) {
                 this.squares.get(y).get(x).addAdjMine();
+            }
         }
     }
+
+    /**
+     * Log the fact that a press was made
+     */
     public void countPress() {
         this.squaresRemaining--;
-        if (this.squaresRemaining == 0) {
+        if (this.squaresRemaining <= 0) {
             this.winGame();
         }
     }
 
+    /**
+     * Lose the game and perform various actions that go along with loss
+     */
     public void loseGame() {
         this.game.getTimer().stop();
-        for (int i = 0; i < this.mines.size(); i++) {
+
+        // display all of the mines
+        for (int i = 0; i < this.mines.size(); i++)
             this.mines.get(i).pushIn();
-        }
 
         this.game.setSmile("loss");
 
         this.playSound("resources/sounds/explosion.wav");
+
+        // popup informing the user of their loss
         JOptionPane.showMessageDialog(this, "You lose! You stepped on a mine!",
                 "Bummer!", JOptionPane.ERROR_MESSAGE, new ImageIcon("resources/images/loss_smiley.png"));
 
         this.isFinished = true;
     }
 
+    /**
+     * Win the game and perform various actions that go along with win
+     */
     public void winGame() {
         this.game.getTimer().stop();
+
         this.game.setSmile("win");
         this.playSound("resources/sounds/cheering.wav");
+
+        // popup informing the user of their win
         JOptionPane.showMessageDialog(this, "You win!", "Nice Job!",
                 JOptionPane.INFORMATION_MESSAGE, new ImageIcon("resources/images/win_smiley.png"));
         this.isFinished = true;
 
+        // check if the user set a new high score
         this.checkHighScore();
     }
 
+    /**
+     * Plays a sound if the game has sound option set to on
+     * @param path - path to the sound file to be played
+     */
     private void playSound(String path) {
+        // if sound setting is false then don't play it
         if (!this.sound)
             return;
 
+        // attempt to play the clip, and catch exceptions
         try {
             AudioInputStream inputStream = AudioSystem.getAudioInputStream(
                     new File(path));
@@ -226,19 +339,28 @@ public class Board extends JPanel {
         }
     }
 
+    /**
+     * Check if the user set a new high score
+     * If so, give them option to save their score and save it
+     */
     private void checkHighScore() {
+        // we don't save high scores for custom board settings
         if (this.difficulty.equals("custom"))
             return;
 
+        // the existing high score for this difficulty
         HighScore existing = this.game.getHighScores().get(this.difficulty);
         int currentTime = this.game.getTimeCount().getTotal();
+
+        // if there is no existing high score, or if the user beat the old high score
         if (existing == null || currentTime < existing.getTime()) {
+
             JPanel panel = new JPanel(new BorderLayout());
 
             String message = "Congrats! You've set the new high score of " + currentTime + " seconds!";
             panel.add(new JLabel(message), BorderLayout.NORTH);
 
-            message = "Please enter your name to save your high score";
+            message = "Please enter your name if you wish to save your high score";
             panel.add(new JLabel(message), BorderLayout.CENTER);
 
             JTextField name = new JTextField();
@@ -250,23 +372,19 @@ public class Board extends JPanel {
                     null, panel, "New High Score!",
                     JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
             if (option == JOptionPane.OK_OPTION) {
+                // if the user clicked ok, save their high score
                 winnerName = name.getText();
                 this.game.getHighScores().logBeginner(currentTime, winnerName);
             }
         }
     }
 
-    public boolean isFinished() {
-        return this.isFinished;
-    }
+    // various getters
+    public boolean isFinished() { return this.isFinished; }
     public boolean isStarted() { return this.isStarted; }
 
-    public void start() {
-        this.isStarted = true;
-        this.game.getTimer().start();
-    }
+    public boolean getHasMarks() { return this.marks; }
+    public boolean getIsColored() { return this.color; }
 
-    public Game getGame() {
-        return this.game;
-    }
+    public Game getGame() { return this.game; }
 }
--- Minesweeper/src/Game.java
+++ Minesweeper/src/Game.java
@@ -1,32 +1,55 @@
+/*
+ * Game.java includes the Game class which is the base JFrame for the application
+ * Author: Emily Pazienza
+ * Created: 08/23/2018
+ */
+
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Game extends JFrame {
 
+    // references to a bunch of components
     private Board board;
     private JButton smile;
-    private String smileImg;
-    private String imgSuff;
-    private NumberSet mineCount, timeCount;
-    private Timer timer;
     private HighScoreList highScores;
+    private NumberSet mineCount, timeCount;
 
+    // the ticking timer which counts the seconds since start
+    private Timer timer;
+
+    // the url of the current smile image to be used
+    private String smileImg;
+    // the image path suffix to be used to determine bw vs color
+    private String imgSuff;
+
+    /**
+     * Creates a new Game
+     * @param title - the title of the game to be sent to the super() call
+     */
     public Game(String title) {
+        // super call for the JFrame
         super(title);
+
         this.getContentPane().setBackground(Color.lightGray);
 
+        // create a new menu bar to line the top of the game
         JMenuBar menuBar = new MenuBar(this);
         this.setJMenuBar(menuBar);
 
+        // JPanel which contains the game part of the frame
         JPanel gamePanel = new JPanel(new BorderLayout(0,10));
 
+        // create a wrapper for the header panel
         JPanel headerWrapper = new JPanel(new BorderLayout());
         headerWrapper.setBackground(Color.lightGray);
+        // panel holds the header components
         JPanel headerPanel = new JPanel(new BorderLayout());
         headerPanel.setBackground(Color.lightGray);
+
+        // cbox is used for proper layout of header components
         Box box = Box.createHorizontalBox();
 
         this.mineCount = new NumberSet();
@@ -34,7 +57,7 @@ public class Game extends JFrame {
 
         box.add(Box.createGlue());
 
-        this.imgSuff = "";
+        this.imgSuff = ""; // default, not black & white, so no suffix
         this.makeSmile();
         box.add(this.smile);
 
@@ -47,39 +70,61 @@ public class Game extends JFrame {
 
         headerPanel.add(box);
 
+        // add some padding/spacing around the header components
         headerPanel.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
         headerWrapper.setBorder(BorderFactory.createLoweredBevelBorder());
         headerWrapper.add(headerPanel);
+
         gamePanel.add(headerWrapper, BorderLayout.NORTH);
 
         this.board = new Board(this);
-        this.board.setBackground(Color.lightGray);
         this.board.setBorder(BorderFactory.createLoweredBevelBorder());
         gamePanel.add(this.board, BorderLayout.CENTER);
 
+        // add some padding around the game
         gamePanel.setBorder(BorderFactory.createEmptyBorder(10, 10, 9, 10));
         gamePanel.setBackground(Color.lightGray);
+
+        // create the HighScoreList
+        this.highScores = new HighScoreList();
+
         this.getContentPane().add(gamePanel, BorderLayout.CENTER);
 
-        this.highScores = new HighScoreList();
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.pack();
     }
+
+    /**
+     * Starts a new custom game with the given options
+     * @param w - the width of the game
+     * @param h - the height of the game
+     * @param n - the number of mines in the game
+     */
     public void newGame(int w, int h, int n) {
         this.setSmile("default");
-        this.board.setCustom(w,h,n);
+        this.board.setDifficulty(w,h,n);
     }
 
+    /**
+     * Starts a new game with a preset difficulty
+     * @param difficulty - String name of the preset
+     */
     public void newGame(String difficulty) {
         this.setSmile("default");
         this.board.setDifficulty(difficulty);
     }
 
+    /**
+     * Starts a new game using the previous settings
+     */
     public void newGame() {
         this.setSmile("default");
         this.board.newGame();
     }
 
+    /**
+     * Creates a new timer for the board which will increment the time counter every second
+     */
     private void createTimer() {
         this.timer = new Timer(1000, new ActionListener() {
             public void actionPerformed(ActionEvent evt) {
@@ -87,22 +132,32 @@ public class Game extends JFrame {
             }});
     }
 
+    /**
+     * Creates the JButton smile component for the game
+     */
     private void makeSmile() {
         JButton sm = new JButton();
+
+        // make sure it maintains 25x25 size
         sm.setPreferredSize(new Dimension(25,25));
         sm.setMaximumSize(new Dimension(25,25));
         sm.setMinimumSize(new Dimension(25,25));
+
         sm.setBackground(Color.lightGray);
         sm.setOpaque(true);
         sm.setBorder(BorderFactory.createRaisedBevelBorder());
-        this.smile = sm;
 
-        this.setSmile("default");
+        this.smile = sm;
+        this.setSmile("default"); // starts at default
         this.smile.addActionListener((ActionEvent event) -> {
             this.newGame();
         });
     }
 
+    /**
+     * Toggles the color option for the board and adjusts the image path suffix
+     * After toggling, resets the smile
+     */
     public void toggleColor() {
         this.board.toggleColor();
         if (this.board.getIsColored()) {
@@ -113,28 +168,46 @@ public class Game extends JFrame {
         this.setSmile(this.smileImg);
     }
 
-    public Board getBoard() {
-        return this.board;
-    }
-
-    public NumberSet getMineCount() {
-        return this.mineCount;
-    }
-
-    public NumberSet getTimeCount() {
-        return this.timeCount;
-    }
-
+    /**
+     * @param name - the filename (discluding path) of the smile image
+     */
     public void setSmile(String name) {
         String path = "resources/images/" + name + "_smiley" + this.imgSuff + ".png";
         this.smileImg = name;
         this.smile.setIcon(new ImageIcon(path));
     }
 
+    /**
+     * @return the Board for the game
+     */
+    public Board getBoard() {
+        return this.board;
+    }
+
+    /**
+     * @return the NumberSet mineCount for the game
+     */
+    public NumberSet getMineCount() {
+        return this.mineCount;
+    }
+
+    /**
+     * @return the NumberSet timeCount for the game
+     */
+    public NumberSet getTimeCount() {
+        return this.timeCount;
+    }
+
+    /**
+     * @return the Timer for the game
+     */
     public Timer getTimer() {
         return this.timer;
     }
 
+    /**
+     * @return the HighScoreList of the game
+     */
     public HighScoreList getHighScores() {
         return this.highScores;
     }
